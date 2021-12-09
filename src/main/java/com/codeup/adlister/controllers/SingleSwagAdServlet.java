280a018c01028034c4248f539bc7c94be71e67cc
MacOS <josemmaldonado50@gmail.com>
2021-12-09 13:46:34 -0600

    created single ad jasp

---
+++ src/main/java/com/codeup/adlister/controllers/SingleSwagAdServlet.java
@@ -0,0 +1,24 @@
+package com.codeup.adlister.controllers;
+
+
+
+import com.codeup.adlister.dao.DaoFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet (name = "controllers.SingleSwagAdServlet", urlPatterns = "/singleSwagAds")
+public class SingleSwagAdServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("ads", DaoFactory.getAdsDao().all());
+        request.getRequestDispatcher("/WEB-INF/ads/singleSwagAd.jsp").forward(request, response);
+    }
+}
+
+
+
---
+++ src/main/webapp/singeSwagAdPage.jsp
@@ -0,0 +1,31 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: macos
+  Date: 12/9/21
+  Time: 11:58 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <jsp:include page="/WEB-INF/partials/head.jsp">
+        <jsp:param name="title" value="Viewing All The Ads" />
+    </jsp:include>
+</head>
+<body>
+<jsp:include page="/WEB-INF/partials/navbar.jsp" />
+
+<div class="container">
+    <h1>Here Are is your ads!</h1>
+
+
+        <div class="col-md-6">
+            <h2>Your ad that you clicked on</h2>
+
+        </div>
+    <
+</div>
+
+</body>
+</html>
