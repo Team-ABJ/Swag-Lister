2fc009bc5219ea1ed0970929756dad4f0effe285
Alex Riley <alex.riley.ar12@gmail.com>
2021-12-13 11:46:36 -0600

    edited user ids on swag to correlate with new users added to database after hashing password

--- Swag-Seeder.sql
+++ Swag-Seeder.sql
@@ -1,5 +1,9 @@
 use adlister_db;
 
+
+
+
+
 INSERT INTO users (username, email, password)
 VALUES ('JoseHustle', 'josehustle2@yahoo.com', 'josehustlehard'),
        ('BurningBernie', 'feelthebern@yahoo.com', 'berningman'),
@@ -7,14 +11,14 @@ VALUES ('JoseHustle', 'josehustle2@yahoo.com', 'josehustlehard'),
        ('RailsOfRuby', 'railingruby@yahoo.com', 'rockingruby');
 
 INSERT INTO swag (user_id, title, description, category, price)
-VALUES ('1', 'Gucci flip flops', 'Brand new Gucci flip flops that have only been worn outside once', 'shoes', '120'),
-       ('1', 'Jordan 11s', 'Like new Jordans with not a mark on them, size 11', 'shoes', '150'),
-       ('1', 'Balenciaga hoodie', 'Never before worn grey Balenciaga hoodie', 'shirts', '350'),
-       ('2', 'Nike Dri-FIT long sleeve', 'Blue Nike long sleeve shirt size Large', 'shirts', '20'),
-       ('2', 'Nike T-shirt', 'Teal Nike Sportswear shirt size Medium', 'shirts', '15'),
-       ('2', 'Texas A&M belt', 'Maroon Mission Belt with Texas A&M logo on buckle', 'accessories', '40'),
-       ('3', 'Bulliant brown belt', 'Brown leather belt poor condition', 'accessories', '40'),
-       ('3', 'Vans belt', 'black and grey checkered Vans belt, good condition', 'accessories', '25'),
+VALUES ('5', 'Gucci flip flops', 'Brand new Gucci flip flops that have only been worn outside once', 'shoes', '120'),
+       ('5', 'Jordan 11s', 'Like new Jordans with not a mark on them, size 11', 'shoes', '150'),
+       ('5', 'Balenciaga hoodie', 'Never before worn grey Balenciaga hoodie', 'shirts', '350'),
+       ('6', 'Nike Dri-FIT long sleeve', 'Blue Nike long sleeve shirt size Large', 'shirts', '20'),
+       ('6', 'Nike T-shirt', 'Teal Nike Sportswear shirt size Medium', 'shirts', '15'),
+       ('6', 'Texas A&M belt', 'Maroon Mission Belt with Texas A&M logo on buckle', 'accessories', '40'),
+       ('6', 'Bulliant brown belt', 'Brown leather belt poor condition', 'accessories', '40'),
+       ('5', 'Vans belt', 'black and grey checkered Vans belt, good condition', 'accessories', '25'),
        ('3', 'Red Nike T-Shirt', 'Red Nike Dri-FIT t-shirt,never before worn', 'shirts', '20'),
        ('4', 'Adidas Superstars', 'Womens size 6 white Superstars with black stripes, brand new condition', 'shoes',
         '55'),
