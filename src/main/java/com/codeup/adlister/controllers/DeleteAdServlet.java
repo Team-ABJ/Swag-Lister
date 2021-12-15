8754c9d83881695770f5ba6df931773d8f757888
Bernie Esquivel <bernie.esquivel01@gmail.com>
2021-12-15 12:20:05 -0600

    feat: updated delete functionality

--- src/main/java/com/codeup/adlister/controllers/DeleteAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/DeleteAdServlet.java
@@ -1,6 +1,8 @@
 package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,14 +17,24 @@ public class DeleteAdServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String id = request.getPathInfo().substring(1);
-        DaoFactory.getAdsDao().destroy(parseInt(id));
+        Long AdId = Long.valueOf(id);
 
+        request.setAttribute("ads", DaoFactory.getAdsDao().findSwagAd(AdId));
+        request.setAttribute("swagId", id);
+        DaoFactory.getAdsDao().destroy(parseInt(id));
         System.out.println(id);
         request.getRequestDispatcher("/WEB-INF/ads/delete.jsp").forward(request, response);
 
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+//        String id = request.getPathInfo().substring(1);
+
+
+        response.sendRedirect("/profile");
 
     }
 }
